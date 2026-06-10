a48f7408de96e9245bf6f759fb1121fda0021e72
JunHee <cyanaria89@gmail.com>
2026-06-10 16:26:02 +0900

    20260610-홍준희/채팅,리뷰,AI - 엔티티, DTO 추가 (message = null 허용)

--- backend/festapick/src/main/java/com/human/festapick/entity/ChatMessages.java
+++ backend/festapick/src/main/java/com/human/festapick/entity/ChatMessages.java
@@ -27,7 +27,7 @@ public class ChatMessages {
   @JoinColumn(name = "user_id", nullable = false)
   private Users user;
 
-  @Column(columnDefinition = "TEXT", nullable = false)
+  @Column(columnDefinition = "TEXT")
   private String message;
 
   @Column(length = 500)
